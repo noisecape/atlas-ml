3a23d0cce5f9cd64c268056e636e3bebe7af5dba
noisecape <tommycaps@hotmai.it>
2024-07-18 23:56:51 +0100

    added basic datapipeline for mnist

--- atlas_ml/datasets/data_pipeline.py
+++ atlas_ml/datasets/data_pipeline.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_diabetes
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from torchvision import datasets, transforms
+
 
 class DataPipeline:
 
@@ -8,12 +9,19 @@ class DataPipeline:
         
         self.configs = configs
 
-    def get_dataset(self, dataset_name:str):
+    def get_dataset(self, dataset_name:str='mnist'):
 
         if dataset_name == 'diabetes':
             return load_diabetes()
         elif dataset_name == 'mnist':
             transform = transforms.ToTensor()
-            dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
-            dataloader = DataLoader(dataset, batch_size=128, shuffle=True)
-            return dataloader
+            trainset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
+            testset = datasets.MNIST(root='./data', train=False,transform=transform, download=True)
+            # calculating the splits
+            train_size = int((1-self.configs['val_split']) * len(trainset))
+            val_size = len(trainset) - train_size
+
+            trainset, valset = random_split(trainset, [train_size, val_size])
+            train_load = DataLoader(trainset, batch_size=self.configs['batch_size'], shuffle=True, num_workers=self.configs['num_workers'], pin_memory=self.configs['pin_memory'])
+            val_load = DataLoader(valset, batch_size=self.configs['batch_size'], shuffle=False, num_workers=self.configs['num_workers'], pin_memory=self.configs['pin_memory'])
+            return train_load, val_load
